512701fda7504f32830c873d4fa19b0ba224315f
Steffen Schwigon <ss5@renormalist.net>
2012-09-06 11:54:41 +0200

    initial Tapper bundle

---
+++ lib/Task/Tapper/Client.pm
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+package Task::Tapper::Client;
+# ABSTRACT: Tapper - dependencies for test machine clients
+
+=pkg App::Bootstrap::Perl
+
+=pkg Tapper::Remote
+
+=pkg Tapper::PRC
+
+=pkg Tapper::Installer
+
+=pkg Tapper::TestSuite::AutoTest
+
+=pkg Tapper::TestSuite::Benchmark::Perl::Formance
+
+=pkg Tapper::TestSuite::Netperf
+
+=pkg Tapper::TestSuite::HWTrack
+
+
+1;
